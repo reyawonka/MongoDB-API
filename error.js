fe19a8a8d3fd4e776d3d007a4ee4d1f0eb366221
sureya <80652954+reyawonka@users.noreply.github.com>
2024-01-08 20:15:02 -0500

    Delete error.js

--- error.js
+++
@@ -1,6 +0,0 @@
-export const errorHandler = (err,req,res,next)=>{
-    const statusCode=res.statusCode;
-    if(statusCode===400){
-        res.json({"title":"missing fields",message: "required fields missing"})
-    }
-};
